3fce2c785f09048229fb00dbca466fb6ee07706f
James Poole <jamesdpoole@icloud.com>
2026-04-27 13:52:28 +1200

    Initial Commit

---
+++ Lure/LureApp.swift
@@ -0,0 +1,17 @@
+//
+//  LureApp.swift
+//  Lure
+//
+//  Created by James Poole on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct LureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
